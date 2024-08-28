3c14a9ed9827b289a9534b1c99800f0a066a3d70
Ahmet Sefa Arşiv <ahmetarsivpm@gmail.com>
2024-08-28 09:46:31 +0300

    Storybook config logo update

--- .storybook/interfeys.theme.ts
+++ .storybook/interfeys.theme.ts
@@ -4,7 +4,7 @@ export default create({
     base: 'light',
     brandTitle: 'INTERFEYS',
     brandUrl: 'https://github.com/codenteq/interfeys',
-    brandImage: 'https://camo.githubusercontent.com/777d1e54aabee835134db2e773713e563c4bbfd9d7c78c48bb279d3c44141aae/68747470733a2f2f636f64656e7465712e636f6d2f77702d636f6e74656e742f75706c6f6164732f323032342f30332f696e746572666579732d6c6f676f2e77656270',
+    brandImage: 'https://codenteq.com/wp-content/uploads/2024/03/interfeys-logo.webp',
     colorPrimary: '#F27A1A',
     colorSecondary: '#273142',
     fontBase: '"Noto Sans", sans-serif',

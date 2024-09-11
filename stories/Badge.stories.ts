547e4f0344d52ada1d6f98731ea2c2d3a38f9a06
Ahmet Sefa Arşiv <ahmetarsivpm@gmail.com>
2024-09-11 16:33:48 +0300

    Badge UI upgrade

--- stories/Badge.stories.ts
+++ stories/Badge.stories.ts
@@ -15,56 +15,28 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
     args: {
-        className: 'bg-blue-100 text-blue-800 text-xs',
-        children: 'DEFAULT',
+        children: 'Badge',
+        variant: 'default',
     },
 };
 
-export const Dark: Story = {
+export const Secondary: Story = {
     args: {
-        className: 'bg-zinc-100 text-zinc-800 text-xs',
-        children: 'DARK',
+        children: 'Badge',
+        variant: 'secondary',
     },
 };
 
-export const Red: Story = {
+export const Destructive: Story = {
     args: {
-        className: 'bg-red-100 text-red-800 text-xs',
-        children: 'RED',
+        children: 'Badge',
+        variant: 'destructive',
     },
 };
 
-export const Green: Story = {
+export const Outline: Story = {
     args: {
-        className: 'bg-green-100 text-green-800 text-xs',
-        children: 'GREEN',
-    },
-};
-
-export const Yellow: Story = {
-    args: {
-        className: 'bg-yellow-100 text-yellow-800 text-xs',
-        children: 'YELLOW',
-    },
-};
-
-export const Indigo: Story = {
-    args: {
-        className: 'bg-indigo-100 text-indigo-800 text-xs',
-        children: 'INDIGO',
-    },
-};
-
-export const Purple: Story = {
-    args: {
-        className: 'bg-purple-100 text-purple-800 text-xs',
-        children: 'PURPLE',
-    },
-};
-
-export const Pink: Story = {
-    args: {
-        className: 'bg-pink-100 text-pink-800 text-xs',
-        children: 'PINK',
+        children: 'Badge',
+        variant: 'outline',
     },
 };

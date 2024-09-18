16ef42dce3050c535a8c32d9cbb3c43387a27af3
Ahmet Sefa Arşiv <ahmetarsivpm@gmail.com>
2024-09-18 17:08:47 +0300

    Radio component deleted

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,6 @@ export { default as CardHeader } from './card/CardHeader';
 export { default as Input } from './input/Input';
 export { default as Label } from './label/Label';
 export { default as Modal } from './modal/Modal';
-export { default as Radio } from './radio/Radio';
 export { default as Select } from './select/Select';
 export { default as Switch } from './switch/Switch';
 export { default as TabsTrigger } from './tab-group/TabsTrigger';
